b3e61d0b2b3a42ff6d848518b46fa43365a9d0c7
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-21 09:03:14 -0600

    Update packages.py

--- lib/packages.py
+++ lib/packages.py
@@ -1,5 +1,6 @@
+link = "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/"
 packages = [
     "http-server", "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/http-server.py", "flask",
     "help", "https://raw.githubusercontent.com/BobReed24/pinecone/refs/heads/main/lib/help.py", "termcolor",
-    "battery", "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/battery.py", "None",
+    "battery", f"{link}battery.py", "psutil",
 ]
